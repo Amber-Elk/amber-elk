661da92f6ad4ae0fa8fd68bbd0b215582d6ef731
Jenna Li <li.jenna@gmail.comclear>
2024-03-19 17:25:42 -0400

    step 2 changes

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
+import Home from './Pages/Pages';
+import MyAccount from './Pages/Pages/MyAccount';
+import MyOrder from './Pages/Pages/MyOrder';
+import MyOrders from './Pages/Pages/MyOrders';
+import NotFound from './Pages/Pages/NotFound';
+import SignIn from './Pages/Pages/SignIn';
 
 function App() {
   return(
     <>
       <div className='bg-gray-500'>Hello World</div>
+      <div className='flex flex-col'>
+        <Home/>
+        <MyAccount/>
+        <MyOrder/>
+        <MyOrders/>
+        <NotFound/>
+        <SignIn/>
+      </div>
     </>
   );
 }
