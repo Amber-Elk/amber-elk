f404c685231062b3f5f312aed201a792091b2317
Bryan Vales <vales.13@osu.edu>
2024-03-20 21:17:34 -0400

    react router

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,3 +1,5 @@
+import {useRoutes, BrowserRouter as Router} from 'react-router-dom';
+
 import './App.css';
 import Home from './Pages/Home';
 import MyAccount from './Pages/MyAccount';
@@ -6,19 +8,22 @@ import MyOrders from './Pages/MyOrders';
 import NotFound from './Pages/NotFound';
 import SignIn from './Pages/SignIn';
 
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home/> },
+    { path: '/my-account', element: <MyAccount/>},
+    { path: '/my-orders', element: <MyOrders/>},
+    { path: '/sign-in', element: <SignIn/>},
+    { path: '*', element: <NotFound/>}
+  ]);
+  return routes;
+};
+
 function App() {
   return(
-    <>
-      <div className='bg-gray-500'>Hello World</div>
-      <div className='flex flex-col'>
-        <Home/>
-        <MyAccount/>
-        <MyOrder/>
-        <MyOrders/>
-        <NotFound/>
-        <SignIn/>
-      </div>
-    </>
+ <Router>
+  <AppRoutes/>
+ </Router>
   );
 }
 
