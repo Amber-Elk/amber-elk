f79161004d2ce152a852143b7488975239ad7763
Jenna Li <li.jenna@gmail.comclear>
2024-03-27 18:58:35 -0400

    Rating and Item

---
+++ src/api/AMBER.ELK.Domain/Catalog/Item.cs
@@ -0,0 +1,47 @@
+namespace AMBER.ELK.Domain.Catalog;
+
+public class Item 
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Brand { get; set; }
+    public decimal Price { get; set; }
+    public List<Rating> Ratings { get; set; } = new List<Rating>();
+
+    public Item(string name, string description, string brand, decimal price)
+    {
+        if(string.IsNullOrWhiteSpace(Name))
+        {
+            throw new ArgumentException("Name is required");
+
+        }
+
+        if(string.IsNullOrWhiteSpace(Description))
+        {
+            throw new ArgumentException("Description is required");
+            
+        }
+
+        if(string.IsNullOrWhiteSpace(Brand))
+        {
+            throw new ArgumentException("Brand is required");
+            
+        }
+
+        if(price < 1)
+        {
+            throw new ArgumentException("Price must be greater than 0");
+        }
+
+        Name = name;
+        Description = description;
+        Brand = brand;
+        Price = price;
+
+    }
+    public void AddRating (Rating rating)
+    {
+        Ratings.Add(rating);
+    }
+}
